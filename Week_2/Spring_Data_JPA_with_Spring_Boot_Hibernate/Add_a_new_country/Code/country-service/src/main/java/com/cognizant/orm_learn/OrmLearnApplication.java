0f965c09948114dad79c547d8053f93225fc72c0
kakumani Abhinay chowdary <abhinaychowdary1111@gmail.com>
2026-07-16 22:43:31 +0530

    new country

---
+++ Week_2/Spring_Data_JPA_with_Spring_Boot_Hibernate/Add_a_new_country/Code/country-service/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,71 @@
+package com.cognizant.orm_learn;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+	private static CountryService countryService;
+
+	public static void main(String[] args) throws CountryNotFoundException {
+
+		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+
+		LOGGER.info("Inside main");
+
+		countryService = context.getBean(CountryService.class);
+
+		testAddCountry();
+	}
+
+	private static void testGetAllCountries() {
+
+		LOGGER.info("Start");
+
+		List<Country> countries = countryService.getAllCountries();
+
+		LOGGER.debug("Countries={}", countries);
+
+		LOGGER.info("End");
+	}
+
+	private static void getCountryTest() throws CountryNotFoundException {
+
+		LOGGER.info("Start");
+
+		Country country = countryService.findCountryByCode("IN");
+
+		LOGGER.debug("Country={}", country);
+
+		LOGGER.info("End");
+	}
+
+	private static void testAddCountry() throws CountryNotFoundException {
+
+		LOGGER.info("Start");
+
+		Country country = new Country();
+		country.setCode("XX");
+		country.setName("Test Country");
+
+		countryService.addCountry(country);
+
+		Country newCountry = countryService.findCountryByCode("XX");
+
+		LOGGER.debug("Country={}", newCountry);
+
+		LOGGER.info("End");
+	}
+}
